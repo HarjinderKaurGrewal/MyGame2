af97308f35a046edf66a7045dbe25a6b31effbc1
HarjinderKaurGrewal <80929413+HarjinderKaurGrewal@users.noreply.github.com>
2021-05-23 18:37:49 +0800

    Room Creation Committed

--- src/main/java/ass3/mygame2/RoomCreation.java
+++ src/main/java/ass3/mygame2/RoomCreation.java
@@ -17,24 +17,37 @@ public class RoomCreation {
     
     private void createRooms() {
 
-        Room castle, kitchen, frontGate;
+        Room castle, kitchen, frontGate,bedroom, garden;
 
         castle = new Room("castle", "You are at the castle", false);
         kitchen = new Room("kitchen", "The kitchen door has a shape of a heart", false);
         frontGate = new Room("frontGate", "There is a giant ogre", true);
+        bedroom = new Room("bedroom", "You are in the master bedroom", true);
+        garden = new Room("garden", "You are in the garden area", true);
         
 
         castle.setExit("east", kitchen);
         castle.setExit("south", frontGate);
+        castle.setExit("north", bedroom);
         frontGate.setExit("north", castle);
+        bedroom.setExit("south", castle);
+        bedroom.setExit("east", garden);
+        garden.setExit("west", bedroom);
+        garden.setExit("south", kitchen);
+        kitchen.setExit("north", garden);
+        kitchen.setExit("west", castle);
 
         castle.addItemInRoom(itemCreation.getItem("excaliburSword"));
         castle.addItemInRoom(itemCreation.getItem("key"));
         kitchen.addItemInRoom(itemCreation.getItem("frontGateKey"));
+        bedroom.addItemInRoom(itemCreation.getItem("pillow"));
+        garden.addItemInRoom(itemCreation.getItem("flower"));
 
         allRoomInGame.add(castle);
         allRoomInGame.add(frontGate);
         allRoomInGame.add(kitchen);
+        allRoomInGame.add(bedroom);
+        allRoomInGame.add(garden);
 
     }
 
